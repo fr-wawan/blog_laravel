be8a8737366691eb6be215abad4ea8db2b4e6cd1
fr-wawan <hermawantan12@gmail.com>
2023-03-30 08:27:11 +0800

    selesai

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,6 @@
 @section('container')
 <div class="p-5 bg-dark m-auto my-5 text-light rounded-3">
     <h1 class="mb-5">Halaman {{$title }}</h1>
-    <h3 class="mb-3">Name : {{ auth()->user()->name }}</h3>
 
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis aperiam illum sunt ut repudiandae ipsum iusto,
         ad earum nesciunt aspernatur magni ullam quo esse aliquid laborum obcaecati beatae, enim quia.</p>
